ee0f763784dd62bf9eb7c14e3d07b290d2935b44
Your Name <you@example.com>
2020-10-25 10:25:14 +0300

    My Django Girls app, first commit

--- movies/admin.py
+++ movies/admin.py
@@ -1,13 +1,166 @@
 from django.contrib import admin
+from django import forms
+from django.utils.safestring import mark_safe
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from .models import Category, Actor, Reviews, Rating, RatingStar, Movie, MovieShots, Genre
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Actor)
-admin.site.register(Reviews)
-admin.site.register(Rating)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+
+class ReviewInline(admin.TabularInline):
+    """Отзывы на странице фильма"""
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")
+
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="110"')
+
+    get_image.short_description = 'Изображение'
+
+
+
+
+
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', "url", 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category_name')
+    inlines = [MovieShotsInline,ReviewInline]
+    actions = ['publish', 'unpublish']
+    save_on_top = True
+    save_as = True
+    list_editable = ("draft",)
+    readonly_fields = ('get_image',)
+    form = MovieAdminForm
+    fieldsets = (
+        (None, {
+            "fields": (("title", "tagline"),)
+        }),
+        (None, {
+            "fields": ("description", ("poster", 'get_image'))
+        }),
+        (None, {
+            "fields": (("year", "world_premiere", "country"),)
+        }),
+        ("Actors", {
+            "classes": ("collapse",),
+            "fields": (("actors", "directors", "genres", "category"),)
+        }),
+        (None, {
+            "fields": (("budget", "fees_in_usa", "fess_in_world"),)
+        }),
+        ("Options", {
+            "fields": (("url", "draft"),)
+        }),
+
+    )
+
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="50" height="60"')
+
+    def unpublish(self, request, queryset):
+        """Снять с публикации"""
+        row_update= queryset.update(draft=True)
+        if row_update == 1:
+            message_bit ='1 запись была обновлена'
+        else:
+            message_bit = f'{row_update} записи были обновлены'
+        self.message_user(request, f'{message_bit}')
+
+    def publish(self,request,queryset):
+        """Опубликовать"""
+        row_update= queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = '1 запись была обновлена'
+        else:
+            message_bit = f'{row_update} записи были обновлены'
+        self.message_user(request, f'{message_bit}')
+
+    publish.short_description = 'Опубликовать'
+    publish.allowed_permissions = ('change',)
+
+    unpublish.short_description = 'Снять с публикации'
+    unpublish.allowed_permissions = ('change',)
+
+
+
+
+    get_image.short_description = 'Постер'
+
+
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    """Актеры и режиссеры"""
+
+
+    list_display = ("name", "age",'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+    get_image.short_description = 'Изображение'
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    """Отзывы"""
+    list_display = ("name", "email", "parent", "movie", "id")
+
+    readonly_fields = ("name", "email")
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Рейтинг"""
+    list_display = ('movie', 'ip')
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    """Кадры из фильма"""
+    list_display = ('title', 'movie', 'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = 'Изображение'
+
+
+
+
+
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Жанры"""
+    list_display = ('name', 'url')
+
+
 admin.site.register(RatingStar)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
-admin.site.register(Genre)
-
-
+admin.site.site_title = 'Top Movies'
+admin.site.site_header = 'Top Movies'
--- movies/views.py
+++ movies/views.py
@@ -1,20 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import  redirect
 from django.views.generic.base import View
-
+from django.views.generic import DetailView, ListView
+from .forms import ReviewForm
 # Create your views here.
 
-from .models import Movie
+from .models import Movie, Category, Actor, Genre
 
-class MoviesView(View):
+class GenreYear:
+    """Жанры и года выходов фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MoviesView(GenreYear,ListView):
     """Список фильмов """
-
-    def get(self,request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {'movie_list': movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(View):
 
-    def get(self,request,slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request,'movies/moviesingle.html',{'movie': movie})
+
+class MovieDetailView(GenreYear,DetailView):
+    model = Movie
+    slug_field = 'url'
+
+
+class AddReview(View):
+    """Отзывы"""
+
+    def post(self,request,pk):
+        form = ReviewForm(request.POST)
+        movie= Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get('parent', None):
+                form.parent_id = int(request.POST.get('parent'))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url() )
+
+
+class ActorView(GenreYear,DetailView):
+    """Вывод информации о актере"""
+
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'
